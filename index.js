90f6752ce0d6e1c368fe071d3fb56620a8f4994a
DevEnforcement <79603553+SirDevEnforcement@users.noreply.github.com>
2021-09-23 14:12:50 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+//-----------------------Packages-------------------------
+const express = require("express");
+const app = express();
+//-------------------Create Server------------------------
+ app.set('trust proxy', true);
+app.use(express.static(__dirname + "/public"), (_, res, next) => {
+  res.status(404)
+  res.sendFile(__dirname + "/")
+  console.log(`ERROR 404 in ${decodeURIComponent(_.path)}`)
+});
+app.listen(3039);
+console.log('Listening on PORT 3039')
