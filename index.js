4be237bbca6be470b9485339ee91137ac46033ea
DevEnforcement <79603553+SirDevEnforcement@users.noreply.github.com>
2021-10-02 09:46:36 +0000

    Scrolling text + FaQ page

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const app = express();
 //-------------------Create Server------------------------
- app.set('trust proxy', true);
+app.set('trust proxy', true);
 app.use(express.static(__dirname + "/public"), (_, res, next) => {
   res.status(404)
   res.sendFile(__dirname + "/")
