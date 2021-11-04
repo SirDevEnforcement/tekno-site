604f2035f2996dab52f6dcff9ff946258ce72549
DevEnforcement <79603553+SirDevEnforcement@users.noreply.github.com>
2021-11-04 16:38:18 +0000

    push before revamp

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //-----------------------Packages-------------------------
 const express = require("express");
 const app = express();
+const router = express.Router();
 //-------------------Create Server------------------------
 app.set('trust proxy', true);
 app.use(express.static(__dirname + "/public"), (_, res, next) => {
@@ -8,5 +9,8 @@ app.use(express.static(__dirname + "/public"), (_, res, next) => {
   res.sendFile(__dirname + "/")
   console.log(`ERROR 404 in ${decodeURIComponent(_.path)}`)
 });
+router.get('/faq', (res, req) => {
+  res.render('faq.ejs')
+})
 app.listen(3039);
 console.log('Listening on PORT 3039')
