8355f3900db060bb5ab9cb934ba3fd93d25af8c0
DevEnforcement <79603553+SirDevEnforcement@users.noreply.github.com>
2021-12-11 12:17:33 +0000

    Update

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
-//-----------------------Packages-------------------------
-const express = require("express");
-const app = express();
-const router = express.Router();
-//-------------------Create Server------------------------
-app.set('trust proxy', true);
-app.use(express.static(__dirname + "/public"), (_, res, next) => {
-  res.status(404)
-  res.sendFile(__dirname + "/")
-  console.log(`ERROR 404 in ${decodeURIComponent(_.path)}`)
-});
-router.get('/faq', (res, req) => {
-  res.render('faq.ejs')
+const express = require("express")
+const app = express()
+const main = process.cwd() + '/public'
+app.use(express.static("public"))
+
+app.get("/", (req, res) => {
+  res.sendFile(main + "/index.html")
 })
-app.listen(3039);
-console.log('Listening on PORT 3039')
+
+app.get("/home", (req, res) => {
+  res.sendFile(main + "/index.html")
+})
+
+app.get("/faq", (req, res) => {
+  res.sendFile(main + "/faq.html")
+})
+
+app.get("/invite", (req, res) => {
+  res.sendFile(main + "/invite.html")
+})
+
+app.get("/nitro", (req, res) => {
+  res.sendFile(main + "/nitro.html")
+})
+
+app.get("/blooket", (req, res) => {
+  res.sendFile(main + "/secret.html")
+})
+
+app.get("/pug", (req, res) => {
+  res.redirect("https://puginarug.tekno-the-bot.repl.co")
+})
+
+app.listen(9999, () => {
+  console.log("web is up!")
+})
